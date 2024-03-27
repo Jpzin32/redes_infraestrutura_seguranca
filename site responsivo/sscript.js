120a81caf0255c51a9a95196b21ef667779496e9
Jonatha <158524181+Jpzin32@users.noreply.github.com>
2024-03-26 21:50:58 -0300

    Add files via upload

---
+++ site responsivo/sscript.js
@@ -0,0 +1,13 @@
+//criar uma criptografia zetapolock
+//quanto mais arrays e splits ter dentro da sua criptografia mais denssa ela é
+function cripto() {
+//td q estiver dentro do parenteses vai ser transformado em getelementbyid 
+let text= document.getElementById('text').value
+//o tor manda criptografias por partes (onion rings) e nop final junta td.
+let splits = text.splits("")
+//depois de cortadas as letras são contadas
+let chars = splits.length
+//
+const pre = ['Z','E','T','T','A', 'P','O','L','A','R', 'z','e','t','t','a', 'p','o','l','a','r']
+//   
+}
